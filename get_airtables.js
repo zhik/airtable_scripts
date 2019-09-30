034183dafb6d744cddac69cb782cebf0697e6e75
zhik <zhikhe@gmail.com>
2019-09-30 16:32:15 -0400

    init tables

---
+++ get_airtables.js
@@ -0,0 +1,57 @@
+const puppeteer = require('puppeteer');
+const env = require('dotenv').config();
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto('https://airtable.com/login?continue=/api');
+
+  const username = await page.$x(
+    '//*[@id="sign-in-form-fields-root"]/div/label[1]/input'
+  );
+  const password = await page.$x(
+    '//*[@id="sign-in-form-fields-root"]/div/label[2]/input'
+  );
+  await username[0].type(process.env.USERNAME);
+  await password[0].type(process.env.PASSWORD);
+
+  await page.waitFor(1000);
+  const submit = await page.$x(
+    '//*[@id="sign-in-form-fields-root"]/div/label[3]/input'
+  );
+
+  await submit[0].click();
+
+  await page.waitForNavigation();
+
+  const links = await page.evaluate(() => {
+    const links = document
+      .querySelector(
+        'body > div > div > div.p3 > div > div:nth-child(2) > div > div:nth-child(1)'
+      )
+      .querySelectorAll('a');
+    return Array.from(links)
+      .map(({ href, text }) => ({
+        href,
+        text
+      }))
+      .filter(link => link.text.toLowerCase().includes('0.3'));
+  });
+
+  await page.goto(links[0].href);
+
+  const tables = await page.evaluate(() => {
+    return window.application.tables.map(
+      ({ columns, sampleRows, isEmpty }) => ({
+        columns: columns.map(column => column.name),
+        sampleRows,
+        isEmpty
+      })
+    );
+  });
+
+  console.log(tables[4].sampleRows[0]);
+
+  //await browser.close();
+})();
